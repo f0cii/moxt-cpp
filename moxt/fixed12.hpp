845762c277024582750d8511e5d1d79352ee2547
yl <2201441955@qq.com>
2024-01-03 07:29:57 +0800

    1

--- moxt/fixed12.hpp
+++ moxt/fixed12.hpp
@@ -127,6 +127,16 @@ const Fixed12 Fixed12Zero = Fixed12::newI(0);
 const Fixed12 Fixed12One = Fixed12::newI(1);
 const Fixed12 Fixed12Two = Fixed12::newI(2);
 
+SEQ_FUNC int64_t fixed_12_int_part(const fixed12_t &fixed);
+
+SEQ_FUNC int64_t fixed_12_frac_part(const fixed12_t &fixed);
+
+SEQ_FUNC fixed12_t fixed_12_new_string_n(const char *cstr, size_t len);
+
+SEQ_FUNC fixed12_t fixed_12_new_string_view(const std::string_view &s);
+
+SEQ_FUNC size_t fixed_12_string_res(const fixed12_t fixed, char *result);
+
 // 自定义*
 SEQ_FUNC int64_t seq_fixed_mul(int64_t a, int64_t b);
 
